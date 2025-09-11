007ce8d2dfbd595946ba1a03ffd6e0985dcc0c46
ladan-13570 <136787148+ladan-13570@users.noreply.github.com>
2025-09-11 07:20:21 -0400

    added basic form handing and typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+new Typewriter('#poem', {
+  strings: ['ern ABAB. Demain, dès l’aube, à l’heure où blanchit la campagne'],
+  autoStart: true,
+  Delay:1,
+  curser:"",
+});}
+
+let poemFormElement = document.querySelector("#poem-generated-form");
+poemFormElement.addEventListener("submit", generatePoem);
