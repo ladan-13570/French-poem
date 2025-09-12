24054cc2f1f40229ae9889a2178fb793b310fcf6
ladan-13570 <136787148+ladan-13570@users.noreply.github.com>
2025-09-12 18:29:41 -0400

    improved CSS and JS

--- src/index.js
+++ src/index.js
@@ -19,8 +19,9 @@ function generatePoem(event) {
   let prompt = `user instructions: Generate a French poem about ${instructionsInput.value}`;
 
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  
   let poemElement = document.querySelector("#poem");
-  poemElement.classList.remove ("hidden");
+  poemElement.classList.remove ("hidden"); 
   poemElement.innerHTML = `<div class="generating">⏳Generating a French poem about ${instructionsInput.value}</div>`;
   console.log("generating poem");
   console.log(`prompt: ${prompt}`);
